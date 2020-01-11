645bafa24ed8aea0df205d5a2bc899aea987afee
Koua Thao <kouathao09@gmail.com>
2020-01-11 01:53:33 -0600

    added pagnation

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,17 +1,51 @@
 import { Injectable } from "@angular/core";
 // 1) import HttpClient
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams
+} from "@angular/common/http";
 
 // error check
 import { throwError } from "rxjs";
-import { retry, catchError } from "rxjs/operators";
+import { retry, catchError, tap } from "rxjs/operators";
 
+// Product interface as the HttpClient.get() call's type parameter in the data service. Go back to the src/app/data.service.ts file and import the Product interface:
+
+import { Product } from "./product";
 @Injectable({
   providedIn: "root"
 })
 export class DataService {
+  // retrieving pagination
+  public first: string = "";
+  public prev: string = "";
+  public next: string = "";
+  public last: string = "";
+
+  // Next, define the parseLinkHeader() method which parses the Link header and populate the previous variables accordingly:
+  parseLinkHeader(header) {
+    if (header.length == 0) {
+      return;
+    }
+
+    let parts = header.split(",");
+    var links = {};
+    parts.forEach(p => {
+      let section = p.split(";");
+      var url = section[0].replace(/<(.*)>/, "$1").trim();
+      var name = section[1].replace(/rel="(.*)"/, "$1").trim();
+      links[name] = url;
+    });
+
+    this.first = links["first"];
+    this.last = links["last"];
+    this.prev = links["prev"];
+    this.next = links["next"];
+  }
+
   // 1)declare private API route
-  private REST_API_SERVER = "http://localhost:3000/products?_page=1";
+  private REST_API_SERVER = "http://localhost:3000/products";
   // 2) pass in construtor
   constructor(private httpClient: HttpClient) {}
 
@@ -32,7 +66,30 @@ export class DataService {
   // 3) get request
   public sendGetRequest() {
     return this.httpClient
-      .get(this.REST_API_SERVER)
-      .pipe(retry(3), catchError(this.handleError));
+      .get<Product[]>(this.REST_API_SERVER, {
+        params: new HttpParams({ fromString: "_page=1&_limit=20" }),
+        observe: "response"
+      })
+      .pipe(
+        retry(3),
+        catchError(this.handleError),
+        tap(res => {
+          console.log(res.headers.get("Link"));
+          this.parseLinkHeader(res.headers.get("Link"));
+        })
+      );
+  }
+  // This method is similar to sendGetRequest() except that it takes the URL to which we need to send an HTTP GET request.
+  public sendGetRequestToUrl(url: string) {
+    return this.httpClient
+      .get<Product[]>(url, { observe: "response" })
+      .pipe(
+        retry(3),
+        catchError(this.handleError),
+        tap(res => {
+          console.log(res.headers.get("Link"));
+          this.parseLinkHeader(res.headers.get("Link"));
+        })
+      );
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 // import data to component
 import { DataService } from "../data.service";
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
+import { HttpResponse } from "@angular/common/http";
+
+import { Product } from "../product";
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -9,16 +14,70 @@ import { DataService } from "../data.service";
 })
 export class HomeComponent implements OnInit {
   // pass in product object
-  products = [];
+  products: Product[] = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   // pass dataservice to constructor
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
-    this.dataService.sendGetRequest().subscribe((data: any[]) => {
-      console.log(data);
-      // When data is received, we added it in the products array.
-      this.products = data;
-    });
+    this.dataService
+      .sendGetRequest()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
+  }
+
+  public firstPage() {
+    this.products = [];
+    this.dataService
+      .sendGetRequestToUrl(this.dataService.first)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+  }
+  public previousPage() {
+    if (this.dataService.prev !== undefined && this.dataService.prev !== "") {
+      this.products = [];
+      this.dataService
+        .sendGetRequestToUrl(this.dataService.prev)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((res: HttpResponse<Product[]>) => {
+          console.log(res);
+          this.products = res.body;
+        });
+    }
+  }
+  public nextPage() {
+    if (this.dataService.next !== undefined && this.dataService.next !== "") {
+      this.products = [];
+      this.dataService
+        .sendGetRequestToUrl(this.dataService.next)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((res: HttpResponse<Product[]>) => {
+          console.log(res);
+          this.products = res.body;
+        });
+    }
+  }
+  public lastPage() {
+    this.products = [];
+    this.dataService
+      .sendGetRequestToUrl(this.dataService.last)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      });
   }
 }
